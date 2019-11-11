ef93bfad8f246feaee828e76a3ecaf2b9c841f6b
Bao Pham <sks@new-host.home>
2019-11-11 12:33:08 -0500

    Hashed

--- api/api-router.js
+++ api/api-router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const bCrypt = require('bcryptjs');
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
@@ -9,5 +9,15 @@ router.use('/users', usersRouter);
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+rourter.post('/hash', (req, res) => {
+  // read a password from the body
+  const password = req.body.password;
+  // hash the password using bcryptjs
+  const hashedPassword = !!password === true && bCrypt.hashSync(password, 15);
+  // return it to the user in an object that looks like
+  res.status(201).json({ password: password, hash: hashedPassword })
+  // { password: 'original passsword', hash: 'hashed password' }
+})
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
 const apiRouter = require('./api-router.js');
-
+const usersRouter = require('../users/users-router');
+const configureMiddleware = require('./configure-middleware');
 const server = express();
 
+configureMiddleware(server);
+
 server.use('/api', apiRouter);
+server.use('/users', usersRouter);
 
 module.exports = server;
